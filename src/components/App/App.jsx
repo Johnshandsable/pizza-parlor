760ba476f0a7ce1adc588859796ce3df5739bc3b
Johnshandsable <Jshands@asu.edu>
2021-02-25 14:53:59 -0600

    added a navigation to app.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,18 +1,30 @@
 import React from 'react';
 import axios from 'axios';
 import './App.css';
+import { HashRouter as Router, Route, Link } from 'react-router-dom';
+import PrimePizza from '../PrimePizza/PrimePizza';
 
 function App() {
-
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <h1 className='App-title'>Prime Pizza</h1>
+    <div className="App">
+      <header className="App-header">
+        <h1 className="App-title">Prime Pizza</h1>
       </header>
-  
-      <img src='images/pizza_photo.png' />
+      <Router>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/prime-pizza-form">PIZZA FORM</Link>
+            </li>
+          </ul>
+        </nav>
+        <Route path="/prime-pizza-form">
+          <PrimePizza />
+        </Route>
+      </Router>
+
+      <img src="images/pizza_photo.png" />
       <p>Pizza is great.</p>
-  
     </div>
   );
 }
---
+++ src/components/PrimePizza/PrimePizza.jsx
@@ -0,0 +1,5 @@
+function PizzaForm() {
+  return <h1>Pizza Form</h1>;
+}
+
+export default PizzaForm;
